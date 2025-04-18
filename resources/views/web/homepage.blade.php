c876a96efd5db2ca4361f919bc047a75d4f471fb
asep1122a <septiyanadam1@gmail.com>
2025-04-18 13:54:45 +0700

    [Database - Menambahkan tabel Products]

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -1,13 +1,20 @@
 <x-layout>
- <x-slot:title>{{ $title }}</x-slot>
-
- <h3>Ini adalah halaman Homepage</h3>
- <br>
-</x-layout>
-<x-card>
-    <x-slot:title>{{ $title }}</x-slot>
-
-    <h3>Ini adalah halaman card</h3>
-</x-card>
-<br>
-<x-alert></x-alert>
+    <div class="row">
+        <h3>Categories</h3>
+        @foreach($categories as $category)
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="{{ $category['image'] }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $category['name'] }}</h5>
+                                <p class="card-text">
+                                    {{ $category['description'] }}
+                                </p>
+                                <a href="/category/{{ $category['slug'] }}" class="btn
+            btn-primary">Detail</a>
+                            </div>
+                        </div>
+                    </div>
+        @endforeach
+    </div>
+</x-layout><x-alert></x-alert>
